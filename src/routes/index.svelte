8697c768d27938b3c4c064bb2e10a581e80884c0
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-10 18:57:44 +0200

    fix: vertical spacing on footer for larger screens

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -54,7 +54,7 @@
 
 <footer class="bg-gray-50 dark:bg-zinc-800 py-16 border-t dark:border-gray-600">
     <div class="container px-10 mx-auto max-w-screen-lg text-gray-500 dark:text-gray-100">
-        <div class="flex flex-col md:flex-row text-center md:text-left space-y-4 justify-between">
+        <div class="flex flex-col md:flex-row text-center md:text-left space-y-4 md:space-y-0 justify-between">
             <p class="text-sm font-extralight">Copyright © 2022 Timo Zacherl. All rights reserved. </p>
             <p><a href="/resume.txt" class="text-sm font-extralight text-gray-800 dark:text-gray-50 hover:text-black border-b border-transparent hover:border-gray-300 dark:hover:text-white transition-colors">Résumé</a></p>
         </div>
