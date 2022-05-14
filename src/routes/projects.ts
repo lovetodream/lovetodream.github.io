859db83debcaa4ce8debd64ee7c26f5c18205f06
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-14 13:06:12 +0200

    feat: change sorting

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -2,7 +2,7 @@ import type { RequestHandlerOutput } from '@sveltejs/kit';
 import { variables } from '$lib/variables';
 
 export async function get(): Promise<RequestHandlerOutput> {
-    const response = await fetch(`${variables.cmsUrl}/api/projects?populate=*`, { headers: { 'Authorization': `Bearer ${variables.cmsApiKey}` } });
+    const response = await fetch(`${variables.cmsUrl}/api/projects?populate=*&sort[0]=id:desc`, { headers: { 'Authorization': `Bearer ${variables.cmsApiKey}` } });
     const data = await response.json();
     return { body: data };
 }
