501e68c6566014f8e1adef44a2b7361d31da7662
Timo <38291523+lovetodream@users.noreply.github.com>
2022-07-19 19:37:27 +0200

    refactor: remove everything

--- src/lib/variables.ts
+++
@@ -1,7 +0,0 @@
-export const variables = {
-    cmsUrl: import.meta.env.VITE_CMS_URL as string,
-    cmsApiKey: import.meta.env.VITE_CMS_API_KEY as string,
-    ghostUrl: import.meta.env.VITE_GHOST_URL as string,
-    ghostKey: import.meta.env.VITE_GHOST_KEY as string,
-    blogUrl: import.meta.env.VITE_BLOG_URL as string,
-}
