27a93b5fdcaac9939a92af2ea0fa7fd323f27a1c
Timo <38291523+lovetodream@users.noreply.github.com>
2022-12-12 20:48:21 +0100

    chore: remove link to outdated cv

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,7 @@
 		<div class="container px-10 mx-auto max-w-screen-lg text-gray-500 dark:text-gray-100">
 			<div class="flex flex-col md:flex-row text-center md:text-left space-y-4 md:space-y-0 justify-between">
 				<p class="text-sm font-extralight">Copyright © 2022 Timo Zacherl. All rights reserved. </p>
-				<p><a href="/resume.txt" class="text-sm font-extralight text-gray-800 dark:text-gray-50 hover:text-black border-b border-transparent hover:border-gray-300 dark:hover:text-white transition-colors">Résumé</a></p>
+				<!-- Not included as of now: <p><a href="/resume.txt" class="text-sm font-extralight text-gray-800 dark:text-gray-50 hover:text-black border-b border-transparent hover:border-gray-300 dark:hover:text-white transition-colors">Résumé</a></p> -->
 			</div>
 		</div>
 	</footer>
