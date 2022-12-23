c5e3141f65d2fcad5c4911f61d96eab49b5654fc
Timo <38291523+lovetodream@users.noreply.github.com>
2022-12-23 12:31:24 +0100

    feat: prerender server side page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,8 @@
 import { variables } from '$lib/variables';
 import type { PageLoad } from './$types';
 
+export const prerender = true
+
 export const load: PageLoad = async () => {
     const response = await fetch(`${variables.cmsUrl}/api/projects?populate=*&sort[0]=id:desc`, { headers: { 'Authorization': `Bearer ${variables.cmsApiKey}` } });
     const data = await response.json();
