3b86bdd68723a5405fa8471f30c5d5e43f510401
Timo <38291523+lovetodream@users.noreply.github.com>
2023-10-17 18:28:33 +0200

    build: improve compile action

--- compile.sh
+++ compile.sh
@@ -1,3 +1,20 @@
+while getopts t: flag
+do
+    case "${flag}" in
+        t) twplatform=${OPTARG};;
+    esac
+done
+
+if [ -n "${twplatform}" ]; then
+    echo "Installing tailwindcss for ${twplatform}..."
+    curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${twplatform}
+    chmod +x ${twplatform}
+    rm -rf tailwindcss
+    mv ${twplatform} tailwindcss
+    echo "Installed tailwindcss sucessfully! :)"
+fi
+
+
 # compile stuff
 ./tailwindcss -i public/assets/pcss/app.pcss -o public/assets/css/app.css --minify
 elm make src/Main.elm --optimize --output=public/main.js
@@ -8,5 +25,4 @@ rm -rf out
 mkdir out
 cp -R public out/public
 rm -rf out/public/pcss
-cp main.js out/main.js
 cp *.html out/
--- update-tailwind-macos.sh
+++
@@ -1,3 +0,0 @@
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-macos-arm64
-chmod +x tailwindcss-macos-arm64
-mv tailwindcss-macos-arm64 tailwindcss
